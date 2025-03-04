70f95ef01579bc97339e668fc1379fca0718f922
Ainacteru <162664298+Ainacteru@users.noreply.github.com>
2025-03-04 14:39:46 -0700

    Update FahrenheitMain.cs
    day 1

--- FahrenheitMain.cs
+++ FahrenheitMain.cs
@@ -4,10 +4,34 @@ public class FahrenheitMain
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("are you gary?");
+        Console.WriteLine("Welcome to my Farhenheit 451 thingy");
+        Console.WriteLine("Type y to say yes, n to say no, type y to continue");
         string answer = Console.ReadLine();
-        if (answer == "n") {
-            Console.WriteLine ("eww");
-        }
+        
+        switch(answer) {
+            case "y":
+                a();
+                break;
+            default:
+                Console.WriteLine("why did you not press y or n, please restart the program");
+                break;
+        } 
+    
+    }
+    
+    public static void a()
+    {
+        Console.WriteLine("please enter your name");
+        
+        string answer = Console.ReadLine();
+        
+        switch(answer) {
+            case "":
+                
+                break;
+            default:
+                a();
+                break;
+        } 
     }
 }
