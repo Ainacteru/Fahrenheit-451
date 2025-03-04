ee775da7ab4ee7b744aca22d963a811176285aa1
Ainacteru <162664298+Ainacteru@users.noreply.github.com>
2025-03-04 14:18:33 -0700

    Create FahrenheitMain.cs
    ew

---
+++ FahrenheitMain.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class FahrenheitMain
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("are you gary?");
+        string answer = Console.ReadLine();
+        if (answer == "n") {
+            Console.WriteLine ("eww");
+        }
+    }
+}
