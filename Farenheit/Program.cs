84ec26dc23f3148571233039b8beed26ab8ca9fa
Ainacteru <162664298+Ainacteru@users.noreply.github.com>
2025-03-05 21:50:50 -0700

    Update Program.cs

--- Farenheit/Program.cs
+++ Farenheit/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,8 +19,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseCors(policy =>
     policy.AllowAnyOrigin()
