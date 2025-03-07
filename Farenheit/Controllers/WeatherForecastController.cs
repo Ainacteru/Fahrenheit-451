f346e4e50a052fdc8f91556d34d37a5f2300fe06
Ainacteru <162664298+Ainacteru@users.noreply.github.com>
2025-03-07 09:42:40 -0700

    trying to make be able to read book

--- Farenheit/Controllers/WeatherForecastController.cs
+++ Farenheit/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace Fahrenheit451API.Controllers
 {
@@ -112,7 +113,31 @@ namespace Fahrenheit451API.Controllers
             }
         }
 
-        
+        private string OpenBook(string input) {
+            try {
+                    //Pass the file path and file name to the StreamReader constructor
+                    StreamReader sr = new StreamReader(input + ".txt");
+                    //Read the first line of text
+                    line = sr.ReadLine();
+                    //Continue to read until you reach end of file
+                    while (line != null)
+                    {
+                        //write the line to console window
+                        Console.WriteLine(line);
+                        //Read the next line
+                        line = sr.ReadLine();
+                    }
+                    //close the file
+                    sr.Close();
+                    Console.ReadLine();
+                }
+            catch(Exception e){
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            finally {
+                    Console.WriteLine("Executing finally block.");
+                }
+        }
 
         private string AvailableBooks(){
             if (limited) {
